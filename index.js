7d2ab59216f0520c55db1a086f88862b27fbc216
ITzDip <62492915+Dip-Bhowmick@users.noreply.github.com>
2021-09-20 20:46:52 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const functions = require("firebase-functions");
 const express = require("express");
 const client = require('twilio')('AC836db151c1e073e1cfb172ee3c8c0440', 'd56628a0b36efdbb505d1870e270832b', { lazyLoading: true });
+
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/timestamp", (req, res) => {
     res.send(`${Date.now()}`);
 });
+
 app.post('/whatsapp', async (req, res) => {
+    console.log(req.body);
     var msg = req.body.Body;
     var to = req.body.From;
     if(msg == "hi"){
@@ -24,5 +27,3 @@ app.post('/whatsapp', async (req, res) => {
     }
     res.end();
 });
-
-exports.app = functions.https.onRequest(app);
